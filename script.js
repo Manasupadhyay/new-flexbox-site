cfe1a41bcd1609dcec2a67adbfbdf33173d96d9b
Manasupadhyay <manasshouter@gmail.com>
2021-07-08 21:42:02 +0530

    04

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const hamburger = document.getElementById('hamburger');
+const header = document.querySelector('header');
 const nav = document.querySelector('nav');
 // const  = document.querySelector('nav ul')
 hamburger.addEventListener('click', function(e){
     // console.log(nav);
     nav.classList.toggle('visible');
     hamburger.classList.toggle('cross');
+    header.classList.toggle('overflow');
 })
 
 nav.addEventListener('click' ,function(e){
